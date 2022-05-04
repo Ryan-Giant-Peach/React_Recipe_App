69e8025ada6ab5dc3a9c2538cd2b20814fa97e0c
Ryan Caulfield <Ryancaulfield3103@gmail.com>
2022-05-04 03:05:53 +0100

    navlink functionality and styling working:

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -7,7 +7,7 @@ function Pages() {
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/cuisine:type" element={<Cuisine />} />
+        <Route path="/cuisine/:type" element={<Cuisine />} />
       </Routes>
 
   );
